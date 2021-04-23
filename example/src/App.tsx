00b052102c2d9500a76678c80732c3d76a1e7145
nokecy <nokecy@163.com>
2021-04-23 18:05:25 +0800

    feat: 添加pod

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import TrtcCalling from 'react-native-trtc-calling';
 
-
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
