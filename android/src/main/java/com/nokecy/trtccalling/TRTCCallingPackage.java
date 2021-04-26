a40e03765bc02c94c7cdd92f49e493d457a83e8e
nokecy <nokecy@163.com>
2021-04-26 11:42:13 +0800

    fix: 类访问级别

--- android/src/main/java/com/nokecy/trtccalling/TRTCCallingPackage.java
+++ android/src/main/java/com/nokecy/trtccalling/TRTCCallingPackage.java
@@ -10,7 +10,7 @@ import java.util.Collections;
 import java.util.List;
 
 
-class TRTCCallingPackage implements ReactPackage {
+public class TRTCCallingPackage implements ReactPackage {
      @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Collections.emptyList();
